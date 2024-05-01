3e18259aae4db5dc982f03189a5255b41a05c257
Aarav12343 <138729396+Aarav12343@users.noreply.github.com>
2024-04-30 22:53:53 -0400

    Add files via upload

---
+++ Room.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyChBrkn5QmSHIl0Am6i5zxhgZ2pmBcIEUo",
+  
+    authDomain: "leoperdlord-gwot.firebaseapp.com",
+  
+    databaseURL: "https://leoperdlord-gwot-default-rtdb.firebaseio.com",
+  
+    projectId: "leoperdlord-gwot",
+  
+    storageBucket: "leoperdlord-gwot.appspot.com",
+  
+    messagingSenderId: "407704911816",
+  
+    appId: "1:407704911816:web:960284d83f09b544fe8afd",
+  
+    measurementId: "G-LF4GWK83Y1"
+  
+  };
+  
+  firebase.initializeApp(firebaseConfig);
